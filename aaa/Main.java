ca166f4503ce38d38a659b1193c05094c8272f10
Emad Salehi <emad.s1178@yahoo.com>
2018-10-11 15:25:55 +0330

    dish sish

--- aaa/Main.java
+++ aaa/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello Fuck!!");
+        System.out.println("Hello Fuck!");
         hey HEY = new hey();
         HEY.badan();
     }
